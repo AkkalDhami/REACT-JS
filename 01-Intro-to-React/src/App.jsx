1aa7663d27f22a4393584cf796de8497c8738f00
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-22 09:11:57 +0545

    conditional rendering in react

--- 01-Intro-to-React/src/App.jsx
+++ 01-Intro-to-React/src/App.jsx
@@ -1,13 +1,40 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <h1>Hello World</h1>
+      <Card name={"Akkal Dhami"} role={"Backend Developer"} exp={2} />
+      <Card name={"John Doe"} role={"Frontend Developer"} exp={5} />
+      <Card name={"Jane Smith"} role={"Full Stack Developer"} exp={8} />
     </>
-  )
+  );
 }
 
-export default App
+const Card = ({ name, role, exp }) => {
+  return (
+    <>
+      <div className="profile-card">
+        <div className="profile-image">
+          <img src="https://i.pravatar.cc/150?img=32" alt="User" />
+        </div>
+        <h2>{name}</h2>
+        <p>{role}</p>
+        <p>Experience: {exp} years</p>
+        <p>{exp > 5 ? "Senior Developer" : "Junior Developer"}</p>
+        <div className="social-icons">
+          <a href="#">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
