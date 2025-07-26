9d1bcb884e59eea577dcfee58df65ccab0f6b0e1
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-26 11:42:37 +0545

    Data Fetching in React: Using Async Await & Try Catch to Fetch Pokémon API

--- 01-Intro-to-React/src/components/hooks/useEffect/HowNotToFetchApi.jsx
+++ 01-Intro-to-React/src/components/hooks/useEffect/HowNotToFetchApi.jsx
@@ -6,17 +6,15 @@ const HowNotToFetchApi = () => {
   const [error, setError] = useState(null);
 
   const fetchData = async () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(err);
-        setLoading(false);
-      });
+    try {
+      let res = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+      const data = await res.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
   };
   useEffect(() => {
     fetchData();
@@ -30,7 +28,13 @@ const HowNotToFetchApi = () => {
       <h1 className="text-4xl">How Not To Fetch Api</h1>
       <ul className="text-lg text-zinc-300 list-disc">
         <img src={data.sprites.other.dream_world.front_default} alt="" />
-        <h1>{data.name}</h1>
+        <h1 className="text-2xl">{data.name}</h1>
+        <div className="mt-4">
+          <p>Weight: {data.weight}</p>
+          <p>Height: {data.height}</p>
+          <p>Speed: {data.stats[0].base_stat}</p>
+          <p>Abilities: {data.abilities[0].ability.name}</p>
+        </div>
       </ul>
     </div>
   );
