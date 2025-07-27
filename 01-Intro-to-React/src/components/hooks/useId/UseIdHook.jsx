401eb382ca57a77acd858ec43aa2fe65503704ab
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-27 16:34:18 +0545

    simplify useId Hook in React

--- 01-Intro-to-React/src/components/hooks/useId/UseIdHook.jsx
+++ 01-Intro-to-React/src/components/hooks/useId/UseIdHook.jsx
@@ -1,27 +1,26 @@
 import { useId } from "react";
 
 const UseIdHook = () => {
-  const nameId = useId();
-  const passwordId = useId();
+  const id = useId();
   return (
     <div>
       <h1 className="text-4xl mb-5">useId Hook</h1>
 
       <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor={nameId}>Name:</label> <br />
+        <label htmlFor={id + "name"}>Name:</label> <br />
         <input
           type="text"
           name="name"
-          id={nameId}
+          id={id + "name"}
           placeholder="Enter name"
           className="p-2 border"
         />
         <br /> <br />
-        <label htmlFor={passwordId}>Password:</label> <br />
+        <label htmlFor={id + "password"}>Password:</label> <br />
         <input
           type="password"
           name="password"
-          id={passwordId}
+          id={id + "password"}
           placeholder="Enter password"
           className="p-2 border"
         />
