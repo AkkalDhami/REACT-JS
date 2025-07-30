9299a01a6b6784389e84b67ef910232f9aabad8a
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-30 09:04:46 +0545

    Set Active Link

--- React-Router/src/components/layout/Header.jsx
+++ React-Router/src/components/layout/Header.jsx
@@ -3,35 +3,40 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const getNavLinkStyles = ({ isActive }) => {
+    return isActive ? { color: "green" } : null;
+  };
   return (
-    <header class="block w-full mx-auto bg-white bg-opacity-90 sticky top-0 shadow backdrop-blur-lg backdrop-saturate-150 z-[9999]">
-      <nav class="w-full max-w-7xl lg:px-8 lg:py-3  px-4 py-2">
-        <div class="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
+    <header className="block font-sans w-full mx-auto bg-white bg-opacity-90 sticky top-0 shadow backdrop-blur-lg backdrop-saturate-150 z-[9999]">
+      <nav className="w-full max-w-7xl lg:px-8 lg:py-3  px-4 py-2">
+        <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
           <NavLink
             href="/"
-            class="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
+            className="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
             Logo
           </NavLink>
-          <div class="hidden lg:block">
-            <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-              <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <NavLink to="/about" class="flex items-center">
+          <div className="hidden lg:block">
+            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "active-link" : "flex items-center"
+                  }>
+                  Home
+                </NavLink>
+              </li>
+              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
+                <NavLink to="/about" style={getNavLinkStyles}>
                   About
                 </NavLink>
               </li>
-              <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <NavLink to="/contact" class="flex items-center">
+              <li className="flex items-center p-1 text-sm gap-x-2 text-slate-600">
+                <NavLink
+                  to="/contact"
+                  className="flex items-center"
+                  style={getNavLinkStyles}>
                   Contact
-                </NavLink>
-              </li>
-              <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <NavLink to="#" class="flex items-center">
-                  Blocks
-                </NavLink>
-              </li>
-              <li class="flex items-center p-1 text-sm gap-x-2 text-slate-600">
-                <NavLink to="#" class="flex items-center">
-                  Docs
                 </NavLink>
               </li>
             </ul>
