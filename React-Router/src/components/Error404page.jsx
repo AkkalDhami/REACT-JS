a4c96ae10f8e99b152a06890c8c01401a5ac3864
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-30 09:22:40 +0545

    useNavigate Hook in React Router

--- React-Router/src/components/Error404page.jsx
+++ React-Router/src/components/Error404page.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { NavLink, useRouteError } from "react-router-dom";
+import { NavLink, useNavigate, useRouteError } from "react-router-dom";
 
 const Error404page = () => {
   const error = useRouteError();
+ const navigate = useNavigate();
   console.log(error);
   return (
     <section className="bg-white dark:bg-gray-900 ">
@@ -32,8 +33,7 @@ const Error404page = () => {
           </p>
 
           <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
-            <NavLink
-              to="/"
+            <button onClick={() => navigate(-1)}
               className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +50,7 @@ const Error404page = () => {
               </svg>
 
               <span>Go back</span>
-            </NavLink>
+            </button>
 
             <NavLink
               to="/"
