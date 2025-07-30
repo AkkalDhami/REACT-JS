4ac5aaa223c9169d770828eca7c1e3e8959e9984
AkkalDhami <dhamiakkal21@gmail.com>
2025-07-30 09:59:31 +0545

    Use ENV Variables to Protect Private Data

--- React-Router/src/api/GetApiData.jsx
+++ React-Router/src/api/GetApiData.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 export const GetApiData = async () => {
-  let url =
-    "https://www.omdbapi.com/?i=tt3896198&apikey=1c12799f&s=titanic&page=1";
+  let url = `https://www.omdbapi.com/?i=tt3896198&apikey=${
+    import.meta.env.VITE_MOVIE_API_KEY
+  }&s=titanic&page=1`;
   try {
     let res = await fetch(url);
     const data = await res.json();
